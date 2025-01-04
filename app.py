25d09e9b014ed4521e5e7c3cc5a4265e1aacbe0a
Ivan_tyh <tanyhivan@gmail.com>
2025-01-04 16:06:32 +0800

    idk if it works

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for  # Flask for creating the web application
+from flask import (Flask, render_template, request, jsonify,
+                   redirect, url_for, flash)  # Flask for creating the web application
 from flask import Blueprint, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
@@ -45,14 +46,12 @@ class SignUpForm(FlaskForm):
     password = PasswordField(validators=[
                              InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
 
-    submit = SubmitField('Register')
+    submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
-        existing_user_username = User.query.filter_by(
-            username=username.data).first()
+        existing_user_username = User.query.filter_by(username=username.data).first()
         if existing_user_username:
-            raise ValidationError(
-                'That username already exists. Please choose a different one.')
+            raise ValidationError('That username already exists. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
@@ -105,13 +104,20 @@ def events():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if Bcrypt.check_password_hash(user.password, form.password.data):
-                login_user(user)
-                return redirect(url_for('home'))
+
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            flash("Login successful!", "success")
+            return redirect(url_for('home'))
+        else:
+            flash("Invalid username or password.", "danger")
+
     return render_template("login.html", form=form)
 
 
@@ -119,7 +125,7 @@ def login():
 def sign_up():
     form = SignUpForm()
     if form.validate_on_submit():
-        hashed_password = Bcrypt.generate_password_hash(form.password.data)
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
         new_user = User(username=form.username.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
